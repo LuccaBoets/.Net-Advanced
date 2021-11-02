00f4385b31d8fa3e77c0926d81706647ade61b50
Lucca Boets <boetslucca@gmail.com>
2021-11-02 09:08:54 +0100

    Added labo

---
+++ Labo 5/Les5Oefenbundel1/Les5Oefenbundel1/Fine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les5Oefenbundel1
+{
+    public class Fine
+    {
+
+        public int Id { get; set; }
+
+        public int PlayerId { get; set; }
+
+        public DateTime DatePayed { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Labo 5/Les5Oefenbundel1/Les5Oefenbundel1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Les5Oefenbundel1
+{
+    public partial class Form1 : Form
+    {
+
+        public List<Player> players { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            players = Program.GetPlayers();
+
+
+        }
+
+        private void knop(object sender, EventArgs e)
+        {
+            var playerFines = players.Select(x => new PlayerFine() { PlayerId = x.Id, PlayerInitials = x.Initials, PlayerName = x.LastName, PlayerStreet = x.Street, PlayerLocation = x.City, PlayerHouseNumber = x.Number, PlayerZipcode = x.ZipCode, NumberOfFines = x.Fines.Count }).ToList();
+            dataGridView1.DataSource = playerFines;
+
+        }
+    }
+}
