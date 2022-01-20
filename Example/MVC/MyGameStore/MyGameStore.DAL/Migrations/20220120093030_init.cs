bd2cdd5ee5772279786a70035aa7d3e9c29443fd
Lucca Boets <boetslucca@gmail.com>
2022-01-20 12:19:56 +0100

    add

---
+++ Example/MVC/MyGameStore/MyGameStore.DAL/Migrations/20220120093030_init.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyGameStore.DAL.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblStores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Number = table.Column<int>(type: "int", maxLength: 5, nullable: false),
+                    Addition = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: true),
+                    ZipCode = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    Place = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
+                    IsFranchiseStore = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblStores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblPeople",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    StoreId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblPeople", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tblPeople_tblStores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "tblStores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblPeople_EmailAddress",
+                table: "tblPeople",
+                column: "EmailAddress",
+                unique: true,
+                filter: "[EmailAddress] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblPeople_StoreId",
+                table: "tblPeople",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblStores_Name",
+                table: "tblStores",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblPeople");
+
+            migrationBuilder.DropTable(
+                name: "tblStores");
+        }
+    }
+}
